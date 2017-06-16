9ef649bf6e13d5b08261144004729382c89c14f9
khanfrd <khanfrd94@gmail.com>
2017-06-16 23:11:51 +0000

    msm8956: remove ANT+ support
     * AOSPA dont have ANT+ support
    
    Change-Id: I122b012baad0c46f35bb409e7acb943a657a47f1
    Signed-off-by: khanfrd <khanfrd94@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -67,15 +67,6 @@ PRODUCT_PACKAGES += \
     charger \
     charger_res_images
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
